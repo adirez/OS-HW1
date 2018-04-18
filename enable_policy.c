9fb24b8b7e806c9bedf975f76b3dd2b38f8e0d97
Shahakbk <shahakbk@gmail.com>
2018-04-18 16:02:34 +0300

    edited syscalls without failure

--- enable_policy.c
+++ enable_policy.c
@@ -3,35 +3,35 @@
 
 int main(int argc, char const *argv[])
 {
-	if(disable_policy(2,234123) != 0){
+	if(disable_policy(1,234123) != 0){
 		printf("1yes\n");
 	} else {
 		printf("1no\n");
 	}
-	if (enable_policy(2, 5, 23412) != 0)
+	if (enable_policy(1, 5, 23412) != 0)
 	{
 		printf("2yes\n");
 	} else {
 		printf("2no\n");
 	}
-	if(enable_policy(2, 5, 234123) == 0){
+	if(enable_policy(1, 5, 234123) == 0){
 		printf("3yes\n");
 	} else {
 		printf("3no\n");
 	}
-	if (enable_policy(2, 5, 234123) != 0)
+	if (enable_policy(1, 5, 234123) != 0)
 	{
 		printf("4yes\n");
 	} else {
 		printf("4no\n");
 	}
-	if (disable_policy(2, 23412) != 0)
+	if (disable_policy(1, 23412) != 0)
 	{
 		printf("5yes\n");
 	} else {
 		printf("5no\n");
 	}
-	if(disable_policy(2, 234123) == 0){
+	if(disable_policy(1, 234123) == 0){
 		printf("6yes\n");
 	} else {
 		printf("6no\n");
--- usr/src/linux-2.4.18-14custom/kernel/enable_policy.c
+++ usr/src/linux-2.4.18-14custom/kernel/enable_policy.c
@@ -7,7 +7,7 @@ int sys_enable_policy(pid_t pid, int size, int password){
 	task_t* task = find_task_by_pid(pid);
 	if(task == NULL) return -ESRCH;
 	if(password != 234123) return -EINVAL;
-	if(task->enabled != 0) return -EINVAL;
+	if(task->enabled == 1) return -EINVAL;
 	if(size < 0) return -EINVAL;
 	task->log = kmalloc(size * sizeof(task->log), GFP_KERNEL);
 	if(task->log == NULL) return -ENOMEM;
